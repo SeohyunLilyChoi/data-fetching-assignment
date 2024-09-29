9d2fd4361af726e50279242ca2d24e61bea51e6e
SeohyunLilyChoi <lily_choi7@snu.ac.kr>
2024-09-29 20:07:33 +0900

    post id, title 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,61 @@
 import './reset.css';
+import './App.css';
 
-import { useReducer } from 'react';
+import { useEffect, useState } from 'react';
+
+type PostResponse = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
 
 export const App = () => {
-  const [count, increment] = useReducer((c: number) => c + 1, 0);
+  const [id, setId] = useState(1);
+  const [data, setData] = useState<PostResponse>();
 
-  return <button onClick={increment}>{count}</button>;
+  useEffect(() => {
+    let ignore = false;
+
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((response) => response.json() as Promise<PostResponse>)
+      .then((response) => {
+        if (ignore) return;
+        setData(response);
+      })
+      .catch(() => {
+        window.alert('실패');
+      });
+
+    return () => {
+      ignore = true;
+    };
+  }, [id]);
+
+  return (
+    <div className="box">
+      <button
+        onClick={() => {
+          setId(id - 1);
+        }}
+      >
+        왼쪽
+      </button>
+      <div className="post-list">
+        <p>포스트 목록</p>
+        {data?.id} {data?.title}
+      </div>
+      <div className="post-details">
+        <p>내용</p>
+        {data?.body}
+      </div>
+      <button
+        onClick={() => {
+          setId(id + 1);
+        }}
+      >
+        오른쪽
+      </button>
+    </div>
+  );
 };
